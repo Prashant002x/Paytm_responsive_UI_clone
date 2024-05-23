aaf9d23a7ad725bd00c68a7a769b9e5cb6bb274d
Prashant002x <prashantsharma6002002@gmail.com>
2024-05-23 13:26:22 +0530

    After changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Paytm_responsive/index.html"],
+  content: ["./index.html"],
   theme: {
     extend: {
       colors:{
