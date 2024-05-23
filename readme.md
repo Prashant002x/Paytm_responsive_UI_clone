9fd1ca2c6c0485084305369c7ce4a0461de9e131
Prashant002x <prashantsharma6002002@gmail.com>
2024-05-23 13:09:07 +0530

    Patym_responsive

---
+++ readme.md
@@ -0,0 +1,10 @@
+#### Author : Prashant Sharma
+
+# Paytm Homepage Clone using Tailwind CSS.
+
+### Duration : 9hrs approax
+[Preview](https://blacktiles.github.io/paytm-clone/)
+
+I Try To Challenge Myself using Tailwind 
+
+![Image Preview](Patym-responsive.png)
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Paytm_responsive/index.html"],
+  theme: {
+    extend: {
+      colors:{
+  bg_color1: "#00BAF2",
+  bg_color2: "#0F4A8A",
+  color1: "#ffffff",
+  color2: "#000",
+  bg_color4: "#0099CC",
+  color4: "#F5F7FA"
+ 
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
